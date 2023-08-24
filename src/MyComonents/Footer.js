d0e2bdbbbfc36e670137eaea91b3495e09958ce5
Akshhay <=>
2023-08-24 12:44:28 +0530

    changed footer commit

--- src/MyComonents/Footer.js
+++ src/MyComonents/Footer.js
@@ -1,23 +1,17 @@
-import React from 'react'
+import React from "react";
 
 function Footer() {
-
   let footerStyle = {
     position: "relative",
     top: "50vh",
-    width: "100%"
-
-  }
-
-
+    width: "100%",
+  };
 
   return (
-    <footer className='bg-dark text-light py-3' >
-      <p className='text-center'>
-      Copyright &copy; MyTodosList.com
-      </p>
+    <footer className="bg-dark text-light py-3">
+      <p className="text-center">Copyright &copy; MyTodosList.com 🔥</p>
     </footer>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
